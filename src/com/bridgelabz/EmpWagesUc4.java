1c2ae63d2854517757039b1342312095db4af02a
Sunil <sunilkumarojha444@gmail.com>
2022-03-21 17:45:46 +0530

    UC-4 add

--- src/com/bridgelabz/EmpWagesUc4.java
+++ src/com/bridgelabz/EmpWagesUc4.java
@@ -1,33 +1,3 @@
 package com.bridgelabz;
 
-import java.util.Random;
 
-public class EmpWagesUc4 {
-    static final int WAGE_PER_HOUR = 20;
-    static final int FULL_DAY_HOUR = 8;
-    static final int PART_TIME_HOUR = 4;
-    static final int IS_PART_TIME = 1;
-    static final int IS_FULL_TIME = 2;
-
-
-    public static void main(String[] args) {
-        Random random = new Random();
-        int empPresent = random.nextInt(3);
-        int empWage = 0;
-        switch (empPresent) {
-            case IS_PART_TIME:
-                empWage = WAGE_PER_HOUR * PART_TIME_HOUR;
-                System.out.println("Daily Emp Wage: " + empWage);
-                break;
-
-            case IS_FULL_TIME:
-                empWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                System.out.println("Daily Emp Wage: " + empWage);
-                break;
-
-            default:
-                empWage = 0;
-                System.out.println("Emp is Absent");
-        }
-    }
-}
